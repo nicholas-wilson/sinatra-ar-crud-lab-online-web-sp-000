6977b10986100e238ad5f789ea218957c7558cb6
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-08 08:56:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,42 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect "/articles/new"
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  post '/articles' do
+    article = Article.create(params)
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    article = Article.find_by_id(params[:id])
+    new_hash = {:title => "#{params[:title]}", :content => "#{params[:content]}"}
+    article.update(new_hash)
+    redirect "articles/#{params[:id]}"
+  end
+
+  delete '/articles/:id' do
+    Article.find_by_id(params[:id]).destroy
+    redirect "/articles"
   end
 end
